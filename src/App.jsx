f780e57956f1fbf1874eb010b2049996bfc4c1b9
hkshakib <humayunkibriashakib@gmail.com>
2024-12-31 13:43:01 +0000

    feat: Add Custom font

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="flex border h-[100%] p-10 m-10">
-      <div className="flex text-[2rem]"></div>
+      <div className="flex justify-center items-center text-[24px] text-green-600 font-heading uppercase">
+        <span>Are you feeling low?</span>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ['"Plus Jakarta Sans"', 'sans-serif'],
+        body: ['"Open Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
